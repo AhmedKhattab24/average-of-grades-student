f11b10b4d24bca29d74ed7b52d4a5c58e33b799f
AhmedKhattab24 <154933305+AhmedKhattab24@users.noreply.github.com>
2023-12-27 01:07:46 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   float mark1= 80;
+    float mark2= 85;
+    float mark3= 75 ;
+    float sum= mark1+mark2+mark3;
+    float result=(sum/300)*100;
+    printf("result=%.f",result);
+    return 0;
+}
